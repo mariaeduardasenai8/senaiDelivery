5f9f19002c340eee6f5d7181f09db2242c1b1323
mariaeduardasenai8 <maria.dudinha88@outlook.com>
2026-07-28 11:06:15 -0300

    Implementado model, migrate e seeder de  categoria

---
+++ database/seeders/CategoriaSedeer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSedeer extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Categoria::create([
+            'nome' => 'Lanches',
+            'descricao' => 'Lanches Diversos',
+            'ativo' => true,
+            'ordem_exibicao' => 1
+        ]);
+           Categoria::create([
+            'nome' => 'Porções',
+            'descricao' => 'Porções Diversas',
+            'ativo' => true,
+            'ordem_exibicao' => 2
+        ]);
+           Categoria::create([
+            'nome' => 'Bebidas',
+            'descricao' => 'Bebidas Diversas',
+            'ativo' => true,
+            'ordem_exibicao' => 3
+        ]);
+    }
+}
